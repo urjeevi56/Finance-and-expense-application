3ba3956928db69c736c5fe48b040354918e5cb35
Prbhat56 <prabhat.kumar@emotorad.com>
2025-12-16 01:43:40 +0530

    fix: added get it dependency

--- lib/init.dart
+++ lib/init.dart
@@ -1,5 +1,7 @@
 import 'package:finanace_and_expense_app/core/services/hive_service.dart';
 import 'package:finanace_and_expense_app/core/theme/cubit/theme_cubit.dart';
+import 'package:finanace_and_expense_app/src/budgets/presentation/bloc/budget_bloc.dart';
+import 'package:finanace_and_expense_app/src/dashboard/presentation/bloc/dashboard_bloc.dart';
 import 'package:finanace_and_expense_app/src/transaction/data/datasource/transaction_local_data_source.dart';
 import 'package:finanace_and_expense_app/src/transaction/data/repositories/transaction_repository_impl.dart';
 import 'package:finanace_and_expense_app/src/transaction/domain/repositories/transaction_repository.dart';
@@ -20,7 +22,11 @@ Future<void> init() async {
   ));
   
  
+    sl.registerFactory(() => DashboardBloc(
+      usecase: sl(),
+  ));
   
+  sl.registerFactory(() => BudgetBloc());
 
   sl.registerLazySingleton(() => TransactionUseCase(sl()));
 
